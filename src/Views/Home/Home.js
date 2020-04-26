9ee7d6039c5e0fff05faa7bd351bf2dc3a5672c1
Anik Mazumder <anik.airworld@gmail.com>
2020-04-26 21:45:27 +0600

    hi

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -5,7 +5,7 @@ import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import {Euro, Search} from "@material-ui/icons";
+import {Search} from "@material-ui/icons";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import Paper from "@material-ui/core/Paper";
 import InputBase from "@material-ui/core/InputBase";
@@ -21,7 +21,6 @@ import CardMedia from "@material-ui/core/CardMedia";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import Backdrop from "@material-ui/core/Backdrop";
 import CircularProgress from "@material-ui/core/CircularProgress";
-import Badge from "@material-ui/core/Badge";
 
 const useStyles = makeStyles((theme) => ({
     heroContent: {
